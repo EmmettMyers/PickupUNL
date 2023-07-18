f847aba1cc3815656b185e0cc0b4040e5e182b5b
Emmett Myers <73912165+EmmettMyers@users.noreply.github.com>
2023-07-18 18:27:16 -0500

    Update authentication.js

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -7,15 +7,6 @@ const socket = io.connect("http://localhost:5000");
 
 // firebase configuration, can't show details
 const firebaseConfig = {
-<<<<<<< HEAD:client/src/services/authentication.js
-    apiKey: "AIzaSyBzFgK46mm-ljicVeO1mzYfOOI_TnepNmg",
-    authDomain: "pickupunl.firebaseapp.com",
-    projectId: "pickupunl",
-    storageBucket: "pickupunl.appspot.com",
-    messagingSenderId: "836685069435",
-    appId: "1:836685069435:web:2b9c8780182765e91636de",
-    measurementId: "G-PTGQDDCKEE"
-=======
   apiKey: " CANNOT SHOW ",
   authDomain: " CANNOT SHOW ",
   projectId: " CANNOT SHOW ",
@@ -23,19 +14,14 @@ const firebaseConfig = {
   messagingSenderId: " CANNOT SHOW ",
   appId: " CANNOT SHOW ",
   measurementId: " CANNOT SHOW "
->>>>>>> 2cafdaf8c2dd0966f6f4257b731e344d99780688:client/src/firebase.js
 };
   
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
-<<<<<<< HEAD:client/src/services/authentication.js
 export function signin(){
-=======
 // fetches google data from login
-document.getElementById("google").addEventListener("click", function() {
->>>>>>> 2cafdaf8c2dd0966f6f4257b731e344d99780688:client/src/firebase.js
     signInWithPopup(auth, provider)
     .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -53,8 +39,3 @@ document.getElementById("google").addEventListener("click", function() {
         const credential = GoogleAuthProvider.credentialFromError(error);
     });
 }
-<<<<<<< HEAD:client/src/services/authentication.js
-=======
-
-export default getUser;
->>>>>>> 2cafdaf8c2dd0966f6f4257b731e344d99780688:client/src/firebase.js
